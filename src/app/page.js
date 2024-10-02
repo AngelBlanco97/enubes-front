7008d34e4ba2cea7b753da10dfaba345e3f70f39
Ángel Blanco Rodríguez <73697059+AngelBlanco97@users.noreply.github.com>
2024-10-02 20:31:07 +0200

    [feat] Home view (basic)

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,41 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to Home!</h1>
-    </div>
+    <section className="">
+      <div className="px-4 py-5 px-md-5 text-center text-lg-start">
+        <div className="container">
+          <div className="row gx-lg-5 align-items-center">
+            <div className="col-lg-6 mb-5 mb-lg-0">
+              <h1 className="my-5 display-3 fw-bold ls-tight">
+                Hotel eNubes
+                <br />
+                <span className="text-primary">Bienvenido</span>
+              </h1>
+              <p className="lead fw-normal text-muted mb-5">
+                ¿Quieres reservar una habitación? ¿O prefieres disfrutar de
+                nuestras ofertas? ¡No te lo pierdas! 🌟 Revisa nuestras
+                habitaciones y disfruta de la mejor experiencia de tu vida.
+              </p>
+              <a
+                className="btn btn-primary btn-lg"
+                href="/habitaciones"
+                role="button"
+              >
+                Ver habitaciones
+              </a>
+            </div>
+            <div className="col-lg-6 mb-5 mb-lg-0">
+              <Image
+                src="/pexels-pixabay-261102.jpg"
+                alt="Imagen de bienvenida"
+                width={1000}
+                height={700}
+                layout="responsive"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
